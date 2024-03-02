6424aef5a76efa6ec6006706e42c9643d9673db8
Aditya4938 <adityamaharshi21@gmail.com>
2024-03-03 00:08:42 +0530

    adding dashboard

--- dashboard/starter-example/app/page.tsx
+++ dashboard/starter-example/app/page.tsx
@@ -1,7 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import Link from 'next/link';
-
+import heroDekstop from '@/public/hero-desktop.png'
+import heroMobile from '@/public/hero-mobile.png'
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -26,6 +28,14 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image src={heroDekstop} width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"/>
+        <Image src={heroMobile} width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshots of the dashboard project showing desktop version"/>
         </div>
       </div>
     </main>
